e2bad269bb5e654df6b5f15c592d142c1f239ce4
Moshood Shoyombo <146624666+Horlar-1st@users.noreply.github.com>
2025-09-06 11:57:54 +0100

    Update Day12.py

--- Day12.py
+++ Day12.py
@@ -2,7 +2,31 @@
 
 # function to count words in a sentence
 def word_counter(sentence):
+    '''
+    Count the number of words in a full sentence.
+
+    This function counts the number of words in a full sentence.
+
+    Parameters
+    ----------
+    text : str
+        A string containing words.
+
+    Returns
+    -------
+    int
+        Number of words in the sentence.
+
+    Examples
+    --------
+    >>> word_counter("How are you doing today?")
+    5
+
+    >>> capitalizer(" economy class flight ")
+    3
+    '''
     return len(sentence.split())
+
 
 # gradio interface for the word counter function 
 # import gradio library
@@ -18,4 +42,5 @@ iwordCounter = gr.Interface(
 )
 
 #  Launch the interface
+
 iwordCounter.launch()                   # set share=True if you want to share the app publicly
