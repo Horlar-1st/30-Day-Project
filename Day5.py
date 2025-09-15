d4df9f8134899ea43c562913f5851b0b78e486c4
Moshood Shoyombo <146624666+Horlar-1st@users.noreply.github.com>
2025-09-15 07:58:18 +0100

    Update Day5.py

--- Day5.py
+++ Day5.py
@@ -4,23 +4,30 @@ import random
 #The guess number
 result = random.randrange(1, 51) 
 print("Welcome to the Number Guessing Game!")
-print("I'm thinking of a number between 1 and 50.")
+print("Think of a number between 1 and 50.")
 
 # First trial input from user
 guess = int(input("Enter your guess: "))
 
-# counts the number of trials
-count = 1
 
-# The loop to check the guess
-while guess != result:
-    if guess < 0 or guess > 50:
-        print(" ❌ The number is not in range! Please try between 1 and 50 only.")
-    elif guess < result:
-        print("❌ Your input is LESSER than the guess!")
-    else:
-        print("❌ Your input is HIGHER than the guess!")
-    guess = int(input("Try another guess: "))
-    count += 1
+# counts
+count = 0
+
+# The loop
+if guess == result:
+        print(f"✅ You did it! You guessed right in just 1 trial 👏🏽")
+else:
+    while guess != result:
+        if guess < 0 or guess > 50:
+            print(" ❌ The number is not in range! Please try between 1 and 50")
+        elif guess < result:
+            print("❌ Your input is LESSER than the guess!")
+        else:
+            print("❌ Your input is HIGHER than the guess!")
+        guess = int(input("Try another guess: "))
+        count += 1
     if guess == result:
-        print(f"✅ Yes, you did it! You guessed right after {count} trials 👏🏽")
+        print(f"✅ You did it! You guessed right after {count+1} trials 👏🏽")
+
+
+# End of program
