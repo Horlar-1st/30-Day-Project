065e27afb53fe9b14a007255b6a02f8e1bcd92bb
Moshood Shoyombo <146624666+Horlar-1st@users.noreply.github.com>
2025-09-18 09:41:45 +0100

    Update Day25.py

--- Day25.py
+++ Day25.py
@@ -48,3 +48,37 @@ class ContactBook:
                     del self.phone_book[key]
                     return f"🗑️ Contact with number {phone} ({key}) has been deleted successfully."
         return "❌ Contact not found."
+
+
+
+# import gradio for creating a web interface
+import gradio as gr
+
+# Create an instance of MyContactBook
+
+MyContactBook = ContactBook()
+
+# Define the Gradio interface
+with gr.Blocks() as demo:
+    gr.Markdown("##  Phone Book")
+    gr.Markdown("Add, delete, or search for contacts by name or phone number.")
+    
+    with gr.Row():
+        name = gr.Textbox(label="Enter name", max_lines=1)
+        phone = gr.Textbox(label="Enter phone number")
+    
+    with gr.Row():
+        add = gr.Button("+ Add")
+        delete = gr.Button("- Delete")
+        search = gr.Button("🔄 Search")
+
+    with gr.Row():
+        result_output = gr.Textbox(label="Result", interactive=False)
+        
+    add.click(fn=MyContactBook.add_contact, inputs=[name, phone], outputs = result_output)
+    delete.click(fn=MyContactBook.delete_contact, inputs=[name, phone], outputs = result_output)
+    search.click(fn=MyContactBook.search_contact, inputs=[name, phone], outputs = result_output)
+
+# gradio launch
+demo.launch()           # To create a public link, set `share=True` in `launch()`.
+
